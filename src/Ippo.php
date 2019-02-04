bc8cfd962f3c4111b96407d0105b6f9b23546c52
leocavalcante <lc@leocavalcante.com>
2019-02-03 22:36:05 -0200

    First draft and its tests

---
+++ src/Ippo.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace LeoCavalcante\Ippo;
+
+use Symfony\Component\Yaml\Yaml;
+
+class Ippo
+{
+    private $config;
+    private $twig;
+
+    static public function fromYaml(string $filename)
+    {
+        return new Ippo(Yaml::parse(file_get_contents($filename)));
+    }
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+        $this->twig = new \Twig_Environment(new \Twig_Loader_Filesystem(__DIR__));
+
+        $this->twig->addFilter(new \Twig_Filter('ucfirst', 'ucfirst'));
+
+        $this->twig->addFilter(new \Twig_Filter('snake_case', function (string $input): string {
+            return strtolower(preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1_$2', $input));
+        }));
+    }
+
+    public function generate(): array
+    {
+        $namespace = $this->config['namespace'];
+
+        return array_map(function (array $definition) use ($namespace) {
+            $keys = array_keys($definition);
+            $vals = array_values($definition);
+
+            $className = $keys[0];
+            $extendsClassName = $vals[0];
+            $attributes = array_combine(array_slice($keys, 1), array_slice($vals, 1));
+
+            $contents = $this->twig->render('ippo.twig', compact('className', 'extendsClassName', 'namespace', 'attributes'));
+
+            return [$className, $contents];
+        }, $this->config['definitions']);
+    }
+}
---
+++ tests/IppoTest.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace LeoCavalcante\Test\Ippo;
+
+use PHPUnit\Framework\TestCase;
+use LeoCavalcante\Ippo\Ippo;
+
+class IppoTest extends TestCase
+{
+    public function testGenerate()
+    {
+        $ippo = Ippo::fromYaml(__DIR__.'/fixture.yaml');
+        $definitions = $ippo->generate();
+
+        foreach ($definitions as $definition) {
+            file_put_contents(__DIR__."/generated/{$definition[0]}.php", $definition[1]);
+        }
+
+        $this->assertFileExists(__DIR__."/generated/User.php");
+
+        require_once __DIR__."/generated/User.php";
+
+        $this->assertTrue(class_exists('\Acme\User'));
+
+        $dt = new \DateTime();
+        $user = new \Acme\User(1, 'username', false, $dt);
+
+        $this->assertTrue(method_exists($user, 'getId'));
+        $this->assertTrue(method_exists($user, 'withId'));
+        $this->assertTrue(method_exists($user, 'getName'));
+        $this->assertTrue(method_exists($user, 'withName'));
+        $this->assertTrue(method_exists($user, 'getIsAdmin'));
+        $this->assertTrue(method_exists($user, 'withIsAdmin'));
+        $this->assertTrue(method_exists($user, 'getBirthDate'));
+        $this->assertTrue(method_exists($user, 'withBirthDate'));
+
+        $this->assertSame(1, $user->getId());
+        $this->assertSame('username', $user->getName());
+        $this->assertSame(false, $user->getIsAdmin());
+        $this->assertSame($dt, $user->getBirthDate());
+
+        $user2 = clone $user;
+
+        $this->assertNotSame($user2, $user);
+        $this->assertEquals($user2, $user);
+        $this->assertSame(1, $user2->getId());
+        $this->assertSame('username', $user2->getName());
+        $this->assertSame(false, $user2->getIsAdmin());
+        $this->assertSame($dt, $user2->getBirthDate());
+
+        $this->assertStringStartsWith("User(\n\tid => 1;\n\tname => \"username\"", (string) $user);
+
+        $userArr = $user->toArray();
+
+        $this->assertTrue(is_array($userArr));
+        $this->assertSame([
+            'id' => 1,
+            'name' => 'username',
+            'is_admin' => false,
+            'birth_date' => $dt,
+        ], $userArr);
+
+        $userJson = json_encode($user);
+        $this->assertStringStartsWith('{"id":1,"name":"username","is_admin":false,"birth_date":{"date":"', $userJson);
+    }
+}
